a2014b43031a873191d9d716221c7af22737a38d
ozerester <ester1228@outlook.co.il>
2022-09-20 18:06:34 +0300

    Stage0_20/09

---
+++ Stage0/Program5705.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System;
+//namespace Stage0;
+partial class Program
+{
+    static void Main()
+    {
+        welcome5705();
+        welcome8631();
+        Console.ReadKey();
+    }
+    private static void welcome5705()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+    private static void welcome8631(){ }
+}
