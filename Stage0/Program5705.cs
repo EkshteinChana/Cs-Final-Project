e7e4af7895be35e5e27f3e703c92b0ad3173e559
visual studio <vs@mbjcomp.org.il>
2022-10-23 15:48:07 +0300

    23/10/2022_C

--- Stage0/Program5705.cs
+++ Stage0/Program5705.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //using System;
-//namespace Stage0;
+namespace Stage0;
 partial class Program
 {
     static void Main()
@@ -9,11 +9,13 @@ partial class Program
         welcome8631();
         Console.ReadKey();
     }
+
+    static partial void welcome8631();
     private static void welcome5705()
     {
         Console.Write("Enter your name: ");
         string name = Console.ReadLine();
         Console.WriteLine("{0}, welcome to my first console application", name);
     }
-    static partial void welcome8631();
+    
 }
