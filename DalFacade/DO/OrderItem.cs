6c76e59b2f2c00cc02a2b0d948b9fcc2c52d53c9
visual studio <vs@mbjcomp.org.il>
2022-10-27 16:18:40 +0300

    OrderItem

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -2,4 +2,30 @@
 namespace DO;
     public struct OrderItem
     {
+        public int ProductId { get; }
+        public int OrderId { get; }
+        public float Price { get; set; }
+        public int Amount { get; set; }
+        public OrderItem(int productId , int orderId ,float price , int amount)
+        {
+            ProductId = productId;
+            OrderId = orderId;
+            Price = price;
+            Amount = amount;
+        }
     }
+
+
+
+//public int ID
+//{
+//    get { return id; }
+//}
+
+//public string ProductName { get; set; }
+//public eCategory Category { get; set; }
+
+//public Product(int Id)
+//{
+//    this.id = Id; // רק כאן אפשר לאתחל את המשתנה
+//}
