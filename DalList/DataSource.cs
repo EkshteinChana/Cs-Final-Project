e3b3cf327ec75e04a8936669b822e533d0506d32
ozerester <ester1228@outlook.co.il>
2022-11-02 19:00:48 +0200

    Wednesday 1

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -39,18 +39,18 @@ internal static class DataSource
 
     private static void CreateProductArr()
     {
-
-        (string, eCategory, double)[] prodNameCategoryPrice = new (string, eCategory, double)[10]
+        // Temp help array 
+        (string, eCategory, double)[] prodNameCategoryPrice = 
            {("Magnet siddur", eCategory.Siddur,52.5 ),
             ("Artistic Shabbat box", eCategory.Shabbat,200),
             ("pendant model siddur", eCategory.Siddur,60),
             ("Artistic status of Passover Haggadot", eCategory.Chaggim,300),
-            ("Zimirot Shabbat lace model", eCategory.Shabbat,16.8),
+            ("Zmiroth Shabbos lace model", eCategory.Shabbat,16.8),
             ("Sukkah decoration", eCategory.Chaggim,99),
             ("tehillim model Pearls", eCategory.Tehillim,70),
             ("prestigious crystal tehillim", eCategory.Tehillim,85),
             ("Mizmor L'todah", eCategory.Others,89),
-            ("An artistic matchbox for Shabbat", eCategory.Shabbat,144)};
+            ("An artistic matchbox for Shabbos", eCategory.Shabbat,144)};
 
         bool notExists;
         int pId;
@@ -58,10 +58,10 @@ internal static class DataSource
         double pPrice;
         int pInStock;
         eCategory pCategory;
-
+        
         for (int i = 0; i < 10; i++)
         {
-            do
+            do      //Rand id and make sure is unique in the array 
             {
                 notExists = true;
                 Random rnd = new Random();
@@ -75,13 +75,14 @@ internal static class DataSource
                     }
                 }
             } while (!notExists);
+
             (pName, pCategory, pPrice) = prodNameCategoryPrice[i];
-            if (i % 20 == 3)
+            if (i % 20 == 0)
                 pInStock = 0;
             else
             {
                 Random rnd = new Random();
-                pInStock = rnd.Next(1, 15);
+                pInStock = rnd.Next(1, 500);
             }
             Product product = new Product(pId, pName, pPrice, pInStock, pCategory);
             productArr[Config.productArrIdx++] = product;
@@ -92,7 +93,7 @@ internal static class DataSource
     private static void CreateOrderArr()
     {
 
-        (string, string, string)[] OrderingDetails = new (string, string, string)[5]
+        (string, string, string)[] CustomerDetails = new (string, string, string)[5]
            {("Shimon Cohen","shimon66@gmail.com", "Shaulzon 66" ),
             ("Daniel Levi","levi05276@gmail.com", "Sorotskin 16"),
             ("Reuven Katz","reuvenkatz123@gmail.com","Idelson 24" ),
@@ -111,33 +112,34 @@ internal static class DataSource
         {
             oId = Config.MaxOrderId;
             Random rnd = new Random();
-            int idx = rnd.Next(0,5);
-            (oCustomerName, oCustomerEmail, oCustomerAddress) = OrderingDetails[idx];
-            rnd = new Random();
+            int idx = rnd.Next(0,5);  // Rand who will be the customer of the current order
+            (oCustomerName, oCustomerEmail, oCustomerAddress) = CustomerDetails[idx];
+            rnd = new Random();//??
             int numDays = rnd.Next(20, 30);
-            TimeSpan daysBefore = TimeSpan.FromDays(numDays);
+            TimeSpan daysBefore = TimeSpan.FromDays(numDays);//????
             oOrderDate = DateTime.Now - daysBefore;
-            if (i%10<8)
+            if (i%10<8)  // 80% have ship date
             {
-                numDays = rnd.Next(1, 5);
-                TimeSpan daysUntulShip = TimeSpan.FromDays(numDays);
-                oShipDate = oOrderDate + daysUntulShip;        
+                numDays = rnd.Next(1, 5);//??
+                TimeSpan daysUntillShip = TimeSpan.FromDays(numDays);
+                oShipDate = oOrderDate + daysUntillShip;
+                if (i % 10 < 6)// 60% from them have delivery date
+                {
+                    numDays = rnd.Next(3, 8);
+                    TimeSpan daysUntilDelivery = TimeSpan.FromDays(numDays);
+                    oDeliveryDate = oShipDate + daysUntilDelivery;
+                }
+                else
+                {
+                    oDeliveryDate = DateTime.MinValue;
+                }
             }
             else
             {
+                oDeliveryDate = DateTime.MinValue;
                 oShipDate = DateTime.MinValue;
             }
-            if (i % 10 < 6)
-            {
-                numDays = rnd.Next(3, 8);
-                TimeSpan daysUntulDelivery = TimeSpan.FromDays(numDays);
-                oDeliveryDate = oShipDate + daysUntulDelivery;
-            }
-            else
-            {
-                oDeliveryDate= DateTime.MinValue; 
-            }
-
+            
             Order order = new Order(oId, oCustomerName, oCustomerEmail, oCustomerAddress, oOrderDate, oShipDate, oDeliveryDate);
             orderArr[Config.orderArrIdx++] = order;
         }
@@ -152,21 +154,35 @@ internal static class DataSource
         double price;
         int amount;
 
-        for (int i = 0; i < orderArr.Length; i++)
+        for (int i = 0; i < orderArr.Length; i++) // Create 1-4 orderItems for each order
         {
             Random rnd = new Random();
-            int numProducts = rnd.Next(1, 5);
-            for(int j=0;j< numProducts; j++)
+            int sumDifProducts = rnd.Next(1, 5); // the sum of the different typs products in the order
+            for(int j=0;j< sumDifProducts; j++)
             {
                 id = Config.MaxOrderItemId;
                 orderId = orderArr[i].id;
-                int pIdx = rnd.Next(0, productArr.Length);
+                bool exist;
+                int pIdx;
+                do
+                {
+                    exist = false;
+                    pIdx = rnd.Next(0, productArr.Length);  // pIdx is the location in the Products array
+                    int pBarcode = productArr[pIdx].id; 
+                        for (int k = 0; k < j ; k++)
+                        {
+                            if(orderItemArr[Config.orderItemArrIdx - k - 1 ].productId == pBarcode)
+                            {
+                                exist = true;
+                            }
+                        }
+                    } while (exist);
                 productId = productArr[pIdx].id;
                 price = productArr[pIdx].price;
                 int amountOfProduct= rnd.Next(1, 10);
                 if(amountOfProduct<= productArr[pIdx].inStock)
                 {
-                    amount = amountOfProduct;
+                    amount = amountOfProduct; // The amount of each product
                 }
                 else
                 {
--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+char choice;
+
+
+void menue(string type)
+{
+    string specialOptions = ".";
+    if(type == "order item") { specialOptions = $@"f for display all the items of existing orders "; };
+    if (type == "order") { specialOptions = $@"f for display all the items in the order
+                                               g for display a specific item of the order "; }
+    Console.WriteLine($@"
+    Choose the following action:
+    a for adding an {type},
+    b for display an {type},
+    c for display the {type}s list,
+    d for updating an {type},
+    e for deleting an {type} from the {type}s list
+    {specialOptions}
+    ");
+    
+    choice = Console.ReadKey().KeyChar;
+    switch( choice)
+    {
+        case 'a': //add
+
+            break;
+    }
+}
+
+void main(){
+    bool toContinue = true;
+    while (toContinue)
+    {
+        Console.WriteLine($@"
+            Press 0 to exit,
+            1 to check the orders list,
+            2 to check the orders list,
+            3 to check the orders items list.
+        ");
+        choice = Console.ReadKey().KeyChar;
+        switch (choice)
+        {
+            case '0':
+                toContinue = false;
+                break;
+            case '1':    
+                menue("order");
+                break;
+
+        }
+    }
+}
+enum options { add = 'a' };
