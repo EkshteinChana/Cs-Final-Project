552be8c370200a30cc855c149d921f02aaf4a310
visual studio <vs@mbjcomp.org.il>
2022-11-16 17:18:38 +0200

    Exceptions & DalApi

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -1,34 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DalApi
+﻿namespace DalApi
 {
-    internal interface ICrud<T> //where T : struct ???
+    public interface ICrud<T> //where T : struct ???
     {
-        
+        int Create(T t);
+        T Read(int id);
+        IEnumerable<T> Read();
+        void Update(T t);
+        void Delete(int id);
     }
 }
-/*int CreateOrder(Order order)
-Order ReadOrder(int id)
-Order[] ReadOrder()
- void UpdateOrder(Order order)
-void DeleteOrder(int id)
 
-/////////
-CreateOrderItem(OrderItem orderItem)
-OrderItem ReadOrderItem(int id)
-OrderItem ReadOrderItem(int pId,int oId)
-OrderItem[] ReadOrderItem()
-OrderItem[] ReadOrderItemByOrderId(int oId)
-void UpdateOrderItem(OrderItem orderItem)
-DeleteOrderItem(int id)
 
-////////
-int CreateProduct(Product product)
- Product ReadProduct(int id)
-Product[] ReadProduct()
-void UpdateProduct(Product product)
-void DeleteProduct(int id)*/
+
+
+
+
+///////// to add 
+//OrderItem ReadOrderItem(int pId,int oId)
+//OrderItem[] ReadOrderItemByOrderId(int oId)
+
---
+++ DalFacade/DalApi/IOrderItem.cs
@@ -0,0 +1,10 @@
+﻿using DO;
+
+namespace DalApi
+{
+    public interface IOrderItem : ICrud<OrderItem>
+    {
+        OrderItem ReadOrderItem(int pId, int oId);
+        IEnumerable<OrderItem> ReadOrderItemByOrderId(int oId);
+    }
+}
