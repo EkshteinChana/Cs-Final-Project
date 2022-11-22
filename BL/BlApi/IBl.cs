6e19617f46c20b0ec895da3a91913c88c74ec6e4
ozerester <ester1228@outlook.co.il>
2022-11-22 17:57:19 +0200

    stage2: begginig of 7 fix order spelling...

--- BL/BlApi/IBl.cs
+++ BL/BlApi/IBl.cs
@@ -9,7 +9,7 @@ namespace BlApi
     public interface IBl
     {
         public IProduct Product { get; }
-        public IOrder Ordrt { get; }
+        public IOrder Order { get; }
         public ICart Cart { get; }
     }
 }
