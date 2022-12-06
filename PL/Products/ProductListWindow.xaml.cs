6a677928b15cd0a677d4e38a14de1398bca52cd4
ozerester <ester1228@outlook.co.il>
2022-12-06 21:46:40 +0200

    stage3 commits to ProductListWindow

--- PL/Products/ProductListWindow.xaml.cs
+++ PL/Products/ProductListWindow.xaml.cs
@@ -23,7 +23,9 @@ namespace PL
     public partial class ProductListWindow : Window
     {
         private IBl bl;
-
+        /// <summary>
+        /// constractor of ProductListWindow which imports the list of products.
+        /// </summary>
         public ProductListWindow(IBl Ibl)
         {
             InitializeComponent();
@@ -32,23 +34,32 @@ namespace PL
             CategorySelector.ItemsSource = Enum.GetValues(typeof(BO.eCategory));
         }
 
+        /// <summary>
+        /// A function that filters the products by category.
+        /// </summary>
         private void CategorySelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             BO.eCategory ctgry=(BO.eCategory)CategorySelector.SelectedItem;
             ProductsListview.ItemsSource = bl.Product.ReadProdsByCategory(ctgry);
         }
-
+        /// <summary>
+        /// A function that opens the ProductWindow for adding a product.
+        /// </summary>
         private void AddProductButton_Click(object sender, RoutedEventArgs e)
         {
             new ProductWindow(bl,null).Show();
         }
-
+        /// <summary>
+        /// A function that opens the ProductWindow for updating or deleting a product.
+        /// </summary>
         private void ProductsListview_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             ProductForList p = (ProductForList)((ListView)sender).SelectedItem;
             new ProductWindow(bl,p.Id).Show();
         }
-
+        /// <summary>
+        /// A function that show all the product
+        /// </summary>
         private void DisplayAllProductsButton_Click(object sender, RoutedEventArgs e)
         {
             ProductsListview.ItemsSource = bl.Product.ReadProdsList();
