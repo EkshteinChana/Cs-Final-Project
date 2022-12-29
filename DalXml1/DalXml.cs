c8966b0968fd9917a3788e29be9f580a5257e7de
ozerester <ester1228@outlook.co.il>
2022-12-29 18:42:18 +0200

    stage6 creating xml files

---
+++ DalXml1/DalXml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+using DalApi;
+using DO;
+
+sealed internal class DalXml : IDal
+{
+
+    private static Lazy<IDal> instance = new Lazy<IDal>(() => new DalXml());
+    public static IDal Instance { get => instance.Value; }
+    private DalXml()
+    {
+        lock (instance)
+        {
+            if (instance == null)
+            {
+                instance = new Lazy<IDal>();
+            }
+        }
+    }
+    public IProduct product { get; } = new Dal.Product();
+    public IOrder order { get; } = new Dal.Order();
+    public IOrderItem orderItem { get; } = new Dal.OrderItem();
+}
+
+
+
