9d8bf0a474f9d63a96a17accc2b5c734d718006f
visual studio <vs@mbjcomp.org.il>
2022-12-29 18:27:25 +0200

    Stage 4 with the final bonuses

--- DalFacade/DalApi/Factory.cs
+++ DalFacade/DalApi/Factory.cs
@@ -2,15 +2,19 @@
 using System.Reflection;
 using static DalApi.DalConfig;
 
+/// <summary>
+/// Here the classes instances of the database is created.
+/// </summary>
 public static class Factory
 {
     public static IDal? Get()
     {
         string dalType = s_dalName
             ?? throw new DalConfigException($"DAL name is not extracted from the configuration");
-        string dal = s_dalPackages[dalType]
-           ?? throw new DalConfigException($"Package for {dalType} is not found in packages list");
-
+        string dal = s_class
+           ?? throw new DalConfigException($"Class for {dalType} is not found in packages list");
+        string dalNamespace = s_namespace
+            ?? throw new DalConfigException($"Namespace for {dalType} is not found in packages list");
         try
         {
             Assembly.Load(dal ?? throw new DalConfigException($"Package {dal} is null"));
@@ -20,7 +24,7 @@ public static class Factory
             throw new DalConfigException("Failed to load {dal}.dll package");
         }
 
-        Type? type = Type.GetType($"Dal.{dal}, {dal}")
+        Type? type = Type.GetType($"{dalNamespace}.{dal}, {dal}")
             ?? throw new DalConfigException($"Class Dal.{dal} was not found in {dal}.dll");
 
         return type.GetProperty("Instance", BindingFlags.Public | BindingFlags.Static)?
