57f9c1ff5942235c7471753813e0828d1ea5a4f7
visual studio <vs@mbjcomp.org.il>
2023-01-01 20:28:13 +0200

    stage6 data for xml

--- DalXml1/Product.cs
+++ DalXml1/Product.cs
@@ -8,7 +8,43 @@ namespace Dal;
 using DalApi;
 using DO;
 
+
 internal class Product : IProduct
 {
-    
+    public Product()
+    {
+        //read
+        //StreamReader product
+    }
+    public int Create(DO.Product t)
+    {
+        throw new NotImplementedException();
+        //write
+        //save
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DO.Product Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<DO.Product> Read(Func<DO.Product, bool>? func = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DO.Product ReadSingle(Func<DO.Product, bool> func)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(DO.Product t)
+    {
+        throw new NotImplementedException();
+    }
 }
