1208cf92f9ce84cc8d18866242e683784ce94a4f
visual studio <vs@mbjcomp.org.il>
2023-01-10 10:40:25 +0200

    dependecy object product

--- PL/Orders/OrderListWindow1.xaml.cs
+++ PL/Orders/OrderListWindow1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,16 +13,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-//namespace PL.Orders
-//{
-//    /// <summary>
-//    /// Interaction logic for OrderForWindow1.xaml
-//    /// </summary>
-//    public partial class OrderForWindow1 : Window
-//    {
-//        public OrderForWindow1()
-//        {
-//            InitializeComponent();
-//        }
-//    }
-//}
+namespace PL.Orders
+{
+    /// <summary>
+    /// Interaction logic for OrderForWindow1.xaml
+    /// </summary>
+        public partial class OrderListWindow : Window { 
+        private IBl bl;
+        public OrderForWindow1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- PL/PO/ProductForList.cs
+++ PL/PO/ProductForList.cs
@@ -1,27 +1,38 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using BO;
+namespace PL.PO
+{
+    /// <summary>
+    /// An entity of product in list
+    /// for product list screen and catalog screen of a manager
+    /// </summary>
+    internal class ProductForList : DependencyObject
+    {
+        public static readonly DependencyProperty idDepenProp = DependencyProperty.Register("Id", typeof(int), typeof(ProductForList));
+        public static readonly DependencyProperty nameDependProp = DependencyProperty.Register("Name", typeof(string), typeof(ProductForList));
+        public static readonly DependencyProperty priceDependProp = DependencyProperty.Register("Price", typeof(double), typeof(ProductForList));
 
-//using BO;
-//namespace PL.PO
-//{
-//    /// <summary>
-//    /// An entity of product in list
-//    /// for product list screen and catalog screen of a manager
-//    /// </summary>
-//    internal class ProductForList : INotifyPropertyChanged
-//    {
-//        public int Id { get; set; }
-//        public string? Name { get; set; }
-//        public double Price { get; set; }
-//        public eCategory? Category { get; set; }
+        public int Id {
+            get { return (int)GetValue(idDepenProp); }
+            set { SetValue(idDepenProp, value); } 
+        }
 
-//        public event PropertyChangedEventHandler? PropertyChanged;
+        public string? Name {
+            get { return }
+            set { SetValue( id, value ); }  
+        }
+        public double Price { get; set; }
+        public eCategory? Category { get; set; }
 
-//        public override string ToString() => $@"
-//        ID: {Id} | name: {Name} | price: {Price} | category: {Category}";
-//    }
-//}
+        
+       
+        public override string ToString() => $@"
+        ID: {Id} | name: {Name} | price: {Price} | category: {Category}";
+    }
+}
