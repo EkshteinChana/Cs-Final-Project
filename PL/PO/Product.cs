7cbe0f4dabd8fd8fc0415960e3ac8f4c0981d86d
visual studio <vs@mbjcomp.org.il>
2023-01-10 13:08:14 +0200

    Product Datacontext

--- PL/PO/Product.cs
+++ PL/PO/Product.cs
@@ -1,33 +1,33 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 
-///// <summary>
-///// Main logical entity of product 
-///// for product detail screens (for manager) 
-///// and actions on a product
-///// </summary>
-//namespace PL.PO;
-//internal class Product : DependencyObject
-//{
+/// <summary>
+/// Main logical entity of product 
+/// for product detail screens (for manager) 
+/// and actions on a product
+/// </summary>
+namespace PL.PO;
+internal class Product : DependencyObject
+{
 
-//    public int Id { get; set; }//product id
-//    public string? Name { get; set; }
-//    public double Price { get; set; }
-//    public BO.eCategory? Category { get; set; }
-//    public int InStock { get; set; }//amount in stock
+    public int Id { get; set; }//product id
+    public string? Name { get; set; }
+    public double Price { get; set; }
+    public BO.eCategory? Category { get; set; }
+    public int InStock { get; set; }//amount in stock
 
-//    public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
 
-//    public override string ToString() => $@"
-//        ID: {Id}
-//        name: {Name} 
-//        price: {Price}
-//        category: {Category}
-//    	amount in stock: {InStock}
-//        ";
-//}
+    public override string ToString() => $@"
+        ID: {Id}
+        name: {Name} 
+        price: {Price}
+        category: {Category}
+    	amount in stock: {InStock}
+        ";
+}
